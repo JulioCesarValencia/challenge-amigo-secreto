48b66f49c331d402f75b3f5807277ac7537ebe01
Julio Cesar Valencia <sesarisuma@gmail.com.com>
2025-09-06 15:29:05 -0500

    crea funcion agregarAmigo v.1

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+
+
+
+function agregarAmigo() {
+    let amigoAAgregar = document.getElementById("amigo").value.trim();
+    if (amigoAAgregar==="") {
+        alert("campo invalido!");
+    } else {
+    amigos.push(amigoAAgregar);
+    limpiarCaja(); 
+    return console.log(amigos);
+    }
+}
+function limpiarCaja() {
+    let valorCaja = document.getElementById("amigo").value = "";
+}
