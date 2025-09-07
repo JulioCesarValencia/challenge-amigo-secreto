dd16f22eabe26981b2e582d9faa5d2e859f61145
Julio Cesar Valencia <sesarisuma@gmail.com.com>
2025-09-07 10:26:53 -0500

    feat:se agrega funcion lista en pantalla y se llama desde la funcion agregar amigo

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
 let amigos = [];
 
+
+//escalabilidad y buena practica
+function asignarTextoElemento(elemento, texto) {
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.textContent = texto;
+    return;
+}
+
+asignarTextoElemento('h1', 'Amigo Secreto');
+asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
 
 
 
@@ -16,33 +28,30 @@ function agregarAmigo() {
             return;
         }    
             amigos.push(amigoAAgregar);
+            listaEnPantalla();
             limpiarCaja();
             console.log(amigos);    
 }
 
 
 
+//muestra lista en pantalla
+function listaEnPantalla() {
+    let lista = document.getElementById("listaAmigos");
+    //limpia la lista existente
+    lista.innerHTML = "";
 
-
-
-
-
-
-
-
+    for (let intentos=0; intentos < amigos.length; intentos++) {
+        let crearLista=document.createElement("li");
+        crearLista.textContent=amigos[intentos];
+        lista.appendChild(crearLista);
+    }
+}
 
 
 
 function limpiarCaja() {
-    let valorCaja = document.getElementById("amigo").value = "";
+    document.getElementById("amigo").value = "";
 }
 
 
-function asignarTextoElemento(elemento, texto) {
-    let elementoHtml = document.querySelector(elemento);
-    elementoHtml.textContent = texto;
-    return;
-}
-
-asignarTextoElemento('h1', 'Amigo Secreto');
-asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
